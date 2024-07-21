6a4da07e39d365635fd24fb0ee0400284620c86c
mcmanyika <mcmanyika@gmail.com>
2024-07-21 16:39:31 -0500

    update

--- src/app/components/Vision.js
+++ src/app/components/Vision.js
@@ -2,13 +2,13 @@ import React from 'react';
 
 const Vision = () => {
   return (
-    <div className="relative parallax  min-h-80 bg-top" style={{ backgroundImage: "url('/images/divaris.jpeg')" }}>
+    <div className="relative parallax  min-h-80 bg-top" style={{ backgroundImage: "url('/images/banner5.png')" }}>
       <div className="absolute inset-0 bg-main opacity-50"></div>
       <div className="relative max-w-7xl mx-auto z-10 flex flex-col md:flex-row items-center justify-between h-full text-white p-4">
         <div className="flex-1 text-center  p-4">
           <h1 className="text-4xl md:text-5xl font-thin">Vision</h1>
           <p className="mt-4 text-xl max-w-3xl font-thin">
-            <b>To be a trailblazer in the provision of quality education.</b> Being a centre for educational excellence nationally, regionally and internationally, offering the best quality in the areas of academics, sports, social and cultural activities.
+            <b>To be a trailblazer in the provision of quality education.</b><br /> Being a centre for educational excellence nationally, regionally and internationally, offering the best quality in the areas of academics, sports, social and cultural activities.
           </p>
         </div>
         <div className="flex-1 text-center  p-4">
