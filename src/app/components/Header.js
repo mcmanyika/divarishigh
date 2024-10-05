e8aa520ebfd982a2bb93c91032de83eb07222677
mcmanyika <mcmanyika@gmail.com>
2024-10-04 21:08:05 -0700

    added apply now button

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -17,6 +17,8 @@ const Header = () => {
   const [showPopover, setShowPopover] = useState(false);
   const popoverRef = useRef(null);
   const [isOverlayVisible] = useGlobalState('isOverlayVisible');
+  const [isOverlay, setIsOverlay] = useState(false);
+  
 
   useEffect(() => {
     const fetchData = async () => {
@@ -85,6 +87,10 @@ const Header = () => {
     setShowPopover(!showPopover);
   };
 
+  const overlayToggle = () => {
+    setIsOverlay(!isOverlay);
+  };
+
   const handleClickOutside = (event) => {
     if (popoverRef.current && !popoverRef.current.contains(event.target)) {
       setShowPopover(false);
@@ -102,7 +108,7 @@ const Header = () => {
       document.removeEventListener('mousedown', handleClickOutside);
     };
   }, [showPopover]);
-
+  
   return (
     <header
       className={`fixed z-50 w-full bg-main text-white transition-all duration-500 ease-in-out ${
@@ -154,41 +160,67 @@ const Header = () => {
         </div> 
       )}
       <nav className="max-w-4xl mx-auto flex justify-between items-center p-4">
-        <div className={`flex items-center space-x-2 ${isOpen ? 'hidden md:flex' : 'block'}`}>
-        {/* <Link href='/'>
-            <Image
-              src="/images/logo.png"
-              alt="Logo"
-              width={50}
-              height={50}
-              className="rounded"
-            />
-          </Link> */}
-          <h1 className="text-sm md:text-2xl font-normal uppercase flex">{schoolName}</h1>
-        </div>
-        {isSticky && (
-          <div className="md:hidden">
-            <button onClick={toggleOverlay} className="text-white focus:outline-none">
-              <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                {isOpen ? (
-                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
-                ) : (
-                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16m-7 6h7" />
-                )}
-              </svg>
+  <div className={`flex items-center space-x-2 ${isOpen ? 'hidden md:flex' : 'block'}`}>
+    <h1 className="text-sm md:text-2xl font-normal uppercase flex">{schoolName}</h1>
+  </div>
+
+  {isSticky && (
+    <div className="md:hidden">
+      <button onClick={toggleOverlay} className="text-white focus:outline-none">
+        <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+          {isOpen ? (
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
+          ) : (
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16m-7 6h7" />
+          )}
+        </svg>
+      </button>
+    </div>
+  )}
+
+  <ul className="hidden md:flex space-x-6 items-center">
+    {titles.length > 0 && titles.map((rw) => (
+      <li key={rw.id} className='pt-4'>
+        <Link href={`${rw.link}`} passHref>
+          <div className="hover:text-gray-300 text-sm font-sans font-thin uppercase pb-2 border-b-2 border-transparent hover:border-white">
+            {rw.title}
+          </div>
+        </Link>
+      </li>
+    ))}
+    <li>
+      <button 
+      onClick={overlayToggle}
+      className="bg-yellow-500 text-white font-bold py-2 px-6 rounded-tr-full rounded-br-full rounded-tl-full rounded-bl-md hover:bg-yellow-600 transition duration-300">
+        APPLY NOW
+      </button>
+    </li>
+  </ul>
+</nav>
+<div
+        className={`fixed top-0 right-0 w-full z-50 h-full bg-main3 transition-transform duration-500 ease-in-out ${isOverlay ? 'translate-x-0' : 'translate-x-full'}`}
+      >
+        <div className="flex items-center justify-center h-full p-10">
+          <div className="text-white text-center">
+            <h2 className="text-2xl md:text-4xl font-thin">Student Application</h2>
+            <p className="mt-4 text-base md:text-lg">As a new student you can now apply online, click below to start the process.</p>
+            <Link href='/admin/dashboard'>
+            <button
+              className="inline-block mt-4 px-6 py-2 bg-main text-white rounded-full transition duration-300"
+            >
+              Apply Now
+            </button>
+            </Link>
+            <button
+              onClick={overlayToggle}
+              className="absolute top-4 right-4 text-white text-xl font-semibold"
+            >
+              &times; {/* Close icon */}
             </button>
           </div>
-        )}
-        <ul className="hidden md:flex space-x-4">
-          {titles.length > 0 && titles.map((rw) => (
-            <li key={rw.id}>
-              <Link href={`${rw.link}`} passHref>
-                <div className="hover:text-gray-300 text-sm font-sans font-thin uppercase pb-2 border-b-2 border-transparent hover:border-white">{rw.title}</div>
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </nav>
+        </div>
+      </div>
+
     </header>
   );
 };
--- src/app/page.js
+++ src/app/page.js
@@ -26,7 +26,6 @@ export default function Home() {
       <Vision />
       <Curriculum />
       <ImageGallery />
-      {/* <NewStudents /> */}
       <Map />
       <ContactUs />
     </Layout>
