5d34d82c530d9c2f5217ac903fd35810ed364321
mcmanyika <mcmanyika@gmail.com>
2024-10-21 00:34:19 -0400

    link tor smart learner

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import Address from "./Address";
 
 const Footer = () => {
@@ -5,7 +6,8 @@ const Footer = () => {
       <footer className="bg-footer text-white text-sm p-4">
         <Address />
         <div className="container mx-auto font-thin text-center">
-        &copy; Copyrights reserved {new Date().getFullYear()}. Developed by <b>LEAPFROG</b> 
+        &copy; Copyrights reserved {new Date().getFullYear()}. Developed by <Link href='https://smartlearner.vercel.app/' target="_blank"><b>LEAPFROG</b> 
+        </Link>
         </div>
       </footer>
     );
