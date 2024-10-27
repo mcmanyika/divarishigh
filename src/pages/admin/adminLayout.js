4c31fb26edb1ca86e435268ae5c50aefeda716e9
mcmanyika <mcmanyika@gmail.com>
2024-10-27 19:30:30 -0400

    update

--- src/pages/admin/adminLayout.js
+++ src/pages/admin/adminLayout.js
@@ -145,7 +145,7 @@ const AdminLayout = ({ children }) => {
               </div>
             )}
           </div>
-          <div className='bg-gray-100'>
+          <div className='w-full bg-gray-100'>
             <Breadcrumb />
             {children}
           </div>
