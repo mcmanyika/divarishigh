f3dccb15c3757f4ab6dfeaacf22b10f0262d9012
mcmanyika <mcmanyika@gmail.com>
2024-11-20 13:30:20 -0600

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,6 +13,7 @@ import { useRef, useEffect, useState } from "react";
 import Image from "next/image";
 import { database } from '../../utils/firebaseConfig';
 import { ref, onValue } from 'firebase/database';
+import Map from '../app/components/Map';
 
 export default function Home() {
   const [images, setImages] = useState<string[]>([]);
@@ -217,14 +218,7 @@ export default function Home() {
               </Card>
               
               <div className="h-[400px] rounded-lg overflow-hidden">
-                <iframe
-                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3024.2219901290355!2d-74.00369674847809!3d40.71277937933185!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c25a23e28c1191%3A0x49f75d3281df052a!2s150%20Park%20Row%2C%20New%20York%2C%20NY%2010007!5e0!3m2!1sen!2sus!4v1647615559175!5m2!1sen!2sus"
-                  width="100%"
-                  height="100%"
-                  style={{ border: 0 }}
-                  allowFullScreen
-                  loading="lazy"
-                ></iframe>
+                <Map />
               </div>
             </div>
             
